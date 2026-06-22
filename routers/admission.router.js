ab1c41f775672b101ef09cc49c89ed115c02bf4a
SamuelProgrammingBest <adewalesamuel835@gmail.com>
2026-06-22 07:15:40 +0100

    Added protected route to admissions collection

--- routers/admission.router.js
+++ routers/admission.router.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const { admissionApply, getAdmissions } = require("../controllers/admission.controller")
+const { verifyFunc } = require("../protected/verify")
 
 const router = express.Router()
 
 router.post("/apply", admissionApply)
-router.get("/get-applications", getAdmissions)
+router.get("/get-applications", verifyFunc, getAdmissions)
 
 
 module.exports = router
